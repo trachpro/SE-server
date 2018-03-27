3050a3c941e6f7ce405289adb4fc97f0bfa4b289
Pham Quang Huy <p.q.huy97@gmail.com>
2018-03-27 15:24:08 +0700

    API get post

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,11 +1,11 @@
-module.exports = function (user_model) {
+module.exports = function (models) {
     return {
         list: (req, res) => {
             var page = req.params.page ? parseInt(req.params.page) : 1;
             var limit = req.params.limit ? parseInt(req.params.limit) : 10;
             if (page < 1) page = 1;
             if (limit < 1 || limit > 20) limit = 10;
-            user_model.findAll({ offset: (page - 1) * limit, limit: limit }).then((datas) => {
+            models.post.findAll({ offset: (page - 1) * limit, limit: limit }).then((datas) => {
                 res.json(datas || [])
             });
         },
@@ -20,28 +20,34 @@ module.exports = function (user_model) {
             if (req.body.age) cond.age = parseInt(req.body.age);
             if (req.body.email) cond.email = req.body.email;
 
-            user_model.findAll({ offset: (page - 1) * limit, limit: limit, where: cond }).then((datas) => {
+            models.post.findAll({ offset: (page - 1) * limit, limit: limit, where: cond }).then((datas) => {
                 res.json(datas || [])
             });
         },
         get: (req, res) => {
             const id = req.params.id;
-            
-            const db = require("../lib/db.js");
-            // user_model.findById(id).then((data) => {
-            //     res.json({ "status": "200", "message": "successful", "data": data.dataValues });
-            // });
-            user_model.findAll({
+            models.post.findOne({
                 where: {id: req.params.id},
+                //add author's atributes
                 include: [{
-                    model: db.user,
+                    model: models.user,
+                    as: 'author',
+                    attributes: ['name'],
+                },{
+                    // commentator's attributes
+                    model: models.comment,
+                    include: [{
+                        model: models.user,
+                        as: 'commentator',
+                        attributes: ['name']
+                    }]
                 }]
             }).then( (data) => {
                 res.json({ "status": "200", "message": "successful", "data": data.dataValues });
             })
         },
         insert: (req, res) => {
-            user_model.create({
+            models.post.create({
                 author_id: req.decoded.id,
                 title: req.body.title,
                 content: req.body.content,
@@ -61,7 +67,7 @@ module.exports = function (user_model) {
                 age: req.body.age,
                 email: req.body.email
             };
-            user_model.update(value, { where: { id: value.id } })
+            models.post.update(value, { where: { id: value.id } })
                 .then((row) => {
                     if (row > 0) {
                         res.json({ "status": "200", "message": row + " row(s) updated", "data": value });
@@ -71,7 +77,7 @@ module.exports = function (user_model) {
                 });
         },
         delete: (req, res) => {
-            user_model.destroy({
+            models.post.destroy({
                 where: { id: req.params.id }
             })
                 .then(rows => {
