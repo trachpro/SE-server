4b4aef24220b9bfdfd32338c8705445b7f42f5eb
Pham Quang Huy <p.q.huy97@gmail.com>
2018-04-07 15:31:12 +0700

    list category

--- app/controllers/category.js
+++ app/controllers/category.js
@@ -1,7 +1,13 @@
 module.exports = function (models) {
     return {
         list: (req, res) => {
-            
+            var page = req.params.page ? parseInt(req.params.page) : 1;	           
+            var limit = req.params.limit ? parseInt(req.params.limit) : 10;	
+            if (page < 1) page = 1;	
+            if (limit < 1 || limit > 20) limit = 10;	
+            models.category.findAll({ offset: (page - 1) * limit, limit: limit }).then((datas) => {	
+                res.json(datas || [])	
+            });
         },
         search: (req, res) => {
            
