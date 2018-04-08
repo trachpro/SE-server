3ae8968bb05ec8241df282d57aada09620636198
Pham Quang Huy <p.q.huy97@gmail.com>
2018-04-08 14:19:24 +0700

    comment apis

--- app/controllers/comment.js
+++ app/controllers/comment.js
@@ -1,43 +1,39 @@
 module.exports = function (models) {
     return {
         list: (req, res) => {
-            var page = req.params.page ? parseInt(req.params.page) : 1;
-            var limit = req.params.limit ? parseInt(req.params.limit) : 10;
-            if (page < 1) page = 1;
-            if (limit < 1 || limit > 20) limit = 10;
-            models.category.findAll({ offset: (page - 1) * limit, limit: limit }).then((datas) => {
-                res.json(datas || [])
-            });
+            
         },
         search: (req, res) => {
-            var page = req.body.page ? parseInt(req.body.page) : 1;
-            var limit = req.body.limit ? parseInt(req.body.limit) : 10;
-            if (page < 1) page = 1;
-            if (limit < 1 || limit > 20) limit = 10;
-
-            var cond = {}
-            if (req.body.name) cond.name = req.body.name;
-            if (req.body.age) cond.age = parseInt(req.body.age);
-            if (req.body.email) cond.email = req.body.email;
-
-            models.category.findAll({ offset: (page - 1) * limit, limit: limit, where: cond }).then((datas) => {
-                res.json(datas || [])
-            });
+            
         },
         get: (req, res) => {
-            const id = req.params.id;
-            models.category.findById(id).then((data) => {
-                res.json({ 
-                    status: true, 
-                    message: successful, 
-                    data: data.dataValues });
+            const ID = req.params.id;
+            models.comment.findById(ID).then((data) => {
+                if(data) {
+                    res.json({ 
+                        status: true, 
+                        message: "successful", 
+                        data: data.dataValues }); 
+                } else {
+                    res.json({
+                        status: false,
+                        message: "Cannot perform action"
+                    })
+                }
+                
+            }).catch( err => {
+                res.json({
+                    status: false,
+                    message: "Cannot perform action"
+                })
             });
         },
         insert: (req, res) => {
-            models.category.create({
-                author_id: req.decoded.id,
-                title: req.body.title,
-                content: req.body.content
+            models.comment.create({
+                postID: req.body.postID,
+                authorID: req.decoded.ID,
+                content: req.body.content,
+                creadAt: Date.now()
             }).then((data) => {
                 res.json({ 
                     status: true, 
@@ -52,38 +48,59 @@ module.exports = function (models) {
         },
         update: (req, res) => {
             var value = {
-                id: req.params.id,
-                name: req.body.name,
-                age: req.body.age,
-                email: req.body.email
+                content: req.body.content,
+                editedAt: Date.now()
             };
-            models.category.update(value, { where: { id: value.id } })
+            var condition = {
+                authorID: req.decoded.ID,
+                ID: req.body.ID
+            }
+            models.comment.update(value, { where: condition })
                 .then((row) => {
                     if (row > 0) {
                         res.json({ 
                             status: true, 
-                            message: row + " row(s) updated", 
-                            data: value });
+                            message: "Comment updated", 
+                            data: value 
+                        });
                     } else {
-                        res.json({ "status": "200", "message": row + " row(s) updated" });
+                        res.json({ 
+                            status: false, 
+                            message: "Update failed" 
+                         });
                     }
+                }).catch((err) => {
+                    res.json({ 
+                        status: false, 
+                        msg: "Cannot perform action" 
+                    });
                 });
         },
         delete: (req, res) => {
-            models.category.destroy({
-                where: { id: req.params.id }
+            var value = {
+                status: 0,
+                deletedAt: Date.now()
+            }
+            models.comment.update({
+                value,
+                where: { ID: req.params.id, authorID: req.decoded.ID }
             })
-                .then(rows => {
-                    if (rows > 0)
+                .then( row => {
+                    if (row > 0)
                         res.json({ 
                             status: true, 
-                            message: rows + " row(s) affected" 
+                            message: "Comment deleted" 
                         });
                     else
                         res.json({ 
                             status: false, 
-                            message: rows + " row(s) affected" 
+                            message: "Cannot delete comment" 
                         });
+                }).catch((err) => {
+                    res.json({ 
+                        status: false, 
+                        msg: "Cannot perform action" 
+                    });
                 });
         }
     }
