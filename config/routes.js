659659d26876f8c61ee88d7f77162d808e5bd251
Pham Quang Huy <p.q.huy97@gmail.com>
2018-05-07 15:18:34 +0700

    get api

--- config/routes.js
+++ config/routes.js
@@ -12,8 +12,8 @@ module.exports = function (app, utils, models) {
     app.get('/api/' + name + '/:limit([0-9]+)/:page([0-9]+)', ctrls[name].list); // get list with page
     app.get('/api/' + name, ctrls[name].list); // get list with default page = 1
     app.post('/api/' + name + '/search', ctrls[name].search); // search
+    app.get('/api/' + name + '/:id([0-9a-f]+)', ctrls[name].get); // get by id
     if (name != 'user') {
-      app.get('/api/' + name + '/:id([0-9a-f]+)', ctrls[name].get); // get by id
       app.post('/api/' + name ,Auth.setMiddleware, ctrls[name].insert); // insert
     }
     app.put('/api/' + name ,Auth.setMiddleware, ctrls[name].update); // update
