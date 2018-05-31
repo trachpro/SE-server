75afd15078be688add3a389059b6ec81810db00c
Pham Quang Huy <p.q.huy97@gmail.com>
2018-06-01 00:03:35 +0700

    test cases for user apis

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var config = require('./config')
     , express = require('express')
     , db = require('./app/lib/db')
     , utils = require('./app/lib/utils')(config, db)
+    , app = express()
    
 function init() {
     db.checkConnection(successHandle, errorHandle);
@@ -10,8 +11,7 @@ function init() {
 function startServer() {
     console.log('Connection has been established successfully.');
     var model_list = utils.loadModels();
-    var app = express()
-        , http = require('http').createServer(app)
+    var http = require('http').createServer(app)
         require('./config/express')(app, config)
         require('./config/routes')(app, utils, model_list)
         http.listen(config.port, function () {
@@ -29,3 +29,4 @@ function errorHandle(err) {
 
 init();
 
+module.exports = app;
--- test/user.js
+++ test/user.js
@@ -1,6 +1,184 @@
-const chai = require('chai'),
+process.env.NODE_ENV = 'local';
+
+const   chai = require('chai'),
         chaiHttp = require('chai-http'),
-        should = chai.should();
+        should = chai.should()
 
 
 chai.use(chaiHttp);
+let baseUrl = "http://localhost:8080/api"
+
+describe("User", () => {
+
+        let login = () => {
+                return new Promise( (resolve, reject) => {
+                        let user = {
+                                username: 'testt',
+                                password: '123456'
+                        }
+                        chai.request(baseUrl)
+                        .post('/login')
+                        .send(user)
+                        .end( (err, res) => {
+                                if (err) {
+                                        let result = new Error({
+                                                status: false,
+                                                message: "Cannot login"
+                                        })
+                                        reject(result);
+                                }
+
+                                if (res.body.status) {
+                                        resolve(res.body);
+                                } else {
+                                        let result = new Error({
+                                                status: false,
+                                                message: "Cannot login"
+                                        })
+                                        reject(result);
+                                }
+                        })
+                })
+                
+        }
+
+        describe("/GET user", () => {
+                it("should return an user", (done) => {
+                        chai.request(baseUrl)
+                                .get('/user/6')
+                                .end( (err, res) => {
+                                        res.should.have.status(200);
+                                        res.body.should.be.a('object');
+                                        res.body.should.have.property('status').eql(true);
+                                        done();
+                                });
+                })
+        })
+        
+        describe("/POST user", () => {
+                it("Should return an error because of username length", done => {
+                        let reqBody = {
+                                username: 'test',
+                                password: '123456',
+                                name: 'Test Account',
+                                email: 'test@test.com'
+                        }
+                        chai.request(baseUrl)
+                        .post('/user')
+                        .send(reqBody)
+                        .end((err, res) => {
+                                if(err) {
+                                        done();
+                                } 
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('status').eql(false);
+                                done();
+                        })
+                })
+
+                it("Should return an error because of missing fields", done => {
+                        let reqBody = {
+                                username: 'admin',
+                                password: '123456'
+                        }
+                        chai.request(baseUrl)
+                        .post('/user')
+                        .send(reqBody)
+                        .end((err, res) => {
+                                if(err) {
+                                        done();
+                                } 
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('status').eql(false);
+                                done();
+                        })
+                })
+                
+                it("Should return an error due to password regex", done => {
+                        let reqBody = {
+                                username: 'admintest',
+                                password: '12 3456',
+                                name: "Testing",
+                                email: "test@test.com.vn"
+                        }
+                        chai.request(baseUrl)
+                        .post('/user')
+                        .send(reqBody)
+                        .end((err, res) => {
+                                if(err) {
+                                        done();
+                                } 
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('status').eql(false);
+                                done();
+                        })
+                })
+
+                it("Should return an error due to email format", done => {
+                        let reqBody = {
+                                username: 'admin',
+                                password: '123456',
+                                name: "Testing",
+                                email: "aWrongEmail"
+                        }
+                        chai.request(baseUrl)
+                        .post('/user')
+                        .send(reqBody)
+                        .end((err, res) => {
+                                if(err) {
+                                        done();
+                                } 
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('status').eql(false);
+                                done();
+                        })
+                })
+        })
+
+        describe("/PUT user", () => {
+                
+                it("Should return an error when not login", done => {
+                        let reqBody = {
+                                name: 'Test'
+                        }
+                        chai.request(baseUrl)
+                        .put('/user')
+                        .send(reqBody)
+                        .end((err,res) => {
+                                if(err) done();
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('status').eql(false);
+                                done();
+                        })
+                })
+
+                it("Should return an error due to an invalid name", done => {
+                        
+                        login()
+                        .then(data => {
+                                let reqBody = {
+                                        name: "A+B",
+                                        token: data.token
+                                }
+                                chai.request(baseUrl)
+                                .put('/user')
+                                .send(reqBody)
+                                .end((err,res) => {
+                                        if(err) done();
+                                        res.should.have.status(200);
+                                        res.body.should.be.a('object');
+                                        res.body.should.have.property('status').eql(false);
+                                        done();
+                                })
+                        })
+                        .catch(err => {
+                                done();
+                        })
+                })
+
+        })
+})
